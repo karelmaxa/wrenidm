edefae2221f22836492bced9ac8cd7103e349128
Karel Maxa <karel.maxa@orchitech.cz>
2026-07-15 13:46:46 +0200

    WIP

--- openidm-zip/src/main/resources/bin/defaults/script/roles/conditionalRoles.js
+++ openidm-zip/src/main/resources/bin/defaults/script/roles/conditionalRoles.js
@@ -12,8 +12,8 @@
  * information: "Portions copyright [year] [name of copyright owner]".
  *
  * Copyright 2016 ForgeRock AS.
+ * Portions Copyright 2026 Wren Security.
  */
-
 
 /**
  * Module which updates conditional role grants for created/updated users/roles. Note that existing grants should
@@ -21,8 +21,8 @@
  * by modifying the members array of the role, or the roles array of the user - it does not invoke openidm to mutate roles.
  */
 (function () {
-    var _ = require('lib/lodash');
-    var relationshipHelper = require('roles/relationshipHelper');
+    const _ = require('lib/lodash');
+    const relationshipHelper = require('roles/relationshipHelper');
 
     /**
      * This function will be called for the onCreate and onUpdate triggers for managed users. It must determine which
@@ -31,11 +31,8 @@
      * @param rolesPropName the name of the array in the user referencing the user's roles
      */
     exports.updateConditionalGrantsForUser = function(user, rolesPropName) {
-        var userRoleGrants,
-            existingConditionalRoles;
-
-        userRoleGrants = relationshipHelper.getConditionalAndDirectGrants(user, rolesPropName, 'user');
-        existingConditionalRoles = relationshipHelper.getConditionalRoles();
+        const userRoleGrants = relationshipHelper.getGrants(user, rolesPropName, 'user');
+        const existingConditionalRoles = openidm.query('managed/role', { _queryFilter: '/condition pr' }).result;
         this.evaluateConditionalRoles(user, rolesPropName, existingConditionalRoles, userRoleGrants);
     };
 
@@ -49,47 +46,65 @@
      * @param user the newly-created, or updated, user
      * @param rolesPropName the name of the array referencing the roles in the user object
      * @param existingConditionalRoles the current set of conditional roles in the system
-     * @param userRoleGrants the current conditional and direct role grants for the user.
+     * @param userRoleGrants the current role grants for the user.
      */
     exports.evaluateConditionalRoles = function(user, rolesPropName, existingConditionalRoles, userRoleGrants) {
-        var conditionalGrants = userRoleGrants.conditionalGrants,
-            directGrants = userRoleGrants.directGrants;
-        user[rolesPropName] =
-            directGrants.concat(
-                _(existingConditionalRoles)
-                    .filter(function (role) {
-                        // find those conditional roles which aren't yet granted to the user in any way
-                        var roleInGrants =
-                            function (grants) {
-                                return _.find(grants, function (grant) {return 'managed/role/' + role._id === grant._ref;}) !== undefined;
-                            };
-                        //only process if the conditional role is not directly or conditionally granted
-                        if (!roleInGrants(directGrants)) {
-                            return !roleInGrants(conditionalGrants) &&
-                                org.forgerock.openidm.condition.Conditions.newCondition(role.condition).evaluate(user, null);
-                        }
-                        return false;
-                    })
-                    .map(function(role) {
-                        // a new relationship representation for those newly granted roles
-                        return {
-                            '_ref': 'managed/role/' + role._id, '_refProperties': {'_grantType' : 'conditional'}
-                        };
-                     })
-                     .value()
-            ).concat(
-                 _.filter(conditionalGrants, function(grant) {
-                     // retain the existing conditional grants which are still valid
-                     var roleCorrespondingToGrant = _.find(existingConditionalRoles, function (role) { return 'managed/role/' + role._id === grant._ref; });
-                     if (roleCorrespondingToGrant === undefined) {
-                         logger.warn("In evaluateConditionalRoles, an existing user grant could not be matched to an " +
-                             "existing conditional role. The grant in question: {}", grant);
-                         return false;
-                     } else {
-                         return org.forgerock.openidm.condition.Conditions.newCondition(roleCorrespondingToGrant.condition).evaluate(user, null);
-                     }
-                 })
-            );
+        const conditionalGrants = userRoleGrants.filter(grant => relationshipHelper.isGrantType(grant, 'conditional'));
+        const otherGrants = userRoleGrants.filter(grant => !relationshipHelper.isGrantType(grant, 'conditional'));
+
+        const conditionalGrantRefs = (conditionalGrants || []).map(grant => grant._ref);
+        const otherGrantRefs = (otherGrants || []).map(grant => grant._ref);
+
+        const result = otherGrants;
+
+        // Add conditional roles that are not assigned yet
+        existingConditionalRoles.filter(role => {
+            const roleRef = 'managed/role/' + role._id;
+            // Check that role is not already granted
+            if (conditionalGrantRefs.includes(roleRef) || otherGrantRefs.includes(roleRef)) {
+                return false;
+            }
+            // Check if the role condition applies to the specified user
+            return org.forgerock.openidm.condition.Conditions.newCondition(role.condition).evaluate(user, null);
+        }).forEach(role => {
+            result.push({
+                _ref: 'managed/role/' + role._id,
+                _refProperties: { _grantType: 'conditional' },
+            });
+        });
+
+        // Retain existing conditional grants that are still valid
+        conditionalGrants.filter(grant => {
+            const role = existingConditionalRoles.find(role => 'managed/role/' + role._id === grant._ref);
+            // Ignore grant for role that is not conditional role anymore
+            if (!role) {
+                return false;
+            }
+            // Check if the role condition still applies to the specified user
+            return org.forgerock.openidm.condition.Conditions.newCondition(role.condition).evaluate(user, null);
+        }).forEach(grant => {
+            result.push(grant);
+        });
+
+        user[rolesPropName] = relationshipHelper.sortGrants(result);
+    }
+
+    /**
+     * Invoked when a role is created. This function will update the members array with user references which enjoy the
+     * role.
+     * @param newRole the newly-created role
+     */
+    exports.roleCreate = function(newRole) {
+        /**
+         * If the newRoles is attempting to update an oldRole with an array of temporalConstraints greater than 1 throw
+         * BadRequestException. As the implementation stands now, we only support one temporal constraint per role.
+         */
+        if (isTemporalConstraintsMultiValue(newRole)) {
+            throw {code : 400, message: "Only 1 temporal constraint is supported per role."}
+        }
+        if (isRoleConditional(newRole)) {
+            newRole.members = resolveConditionalRoleMembers(newRole);
+        }
     }
 
     /**
@@ -111,204 +126,69 @@
          role condition has changed. And if the role's condition has been removed, the new role grantees will be only
          those members who previously enjoyed a direct grant.
          */
-        if (relationshipHelper.isRoleConditional(newRole) && hasRoleConditionChanged(oldRole, newRole)) {
-            newRole.members = processUpdatedConditionalRole(oldRole, newRole);
-        } else if (relationshipHelper.isRoleConditional(oldRole) && !relationshipHelper.isRoleConditional(newRole)) {
-            newRole.members = processRoleConditionRemoval(oldRole);
+        if (isRoleConditional(newRole) && !_.isEqual(oldRole.condition, newRole.condition)) {
+            newRole.members = resolveConditionalRoleMembers(newRole);
+        } else if (isRoleConditional(oldRole) && !isRoleConditional(newRole)) {
+            newRole.members = relationshipHelper.sortGrants(relationshipHelper.getGrants(oldRole, 'members', 'role')
+                .filter(grant => !relationshipHelper.isGrantType(grant, 'conditional')));
         }
     }
 
     /**
-     * Invoked when a role is created. This function will update the members array with user references which enjoy the
-     * role.
-     * @param newRole the newly-created role
+     * TODO FIXME
      */
-    exports.roleCreate = function(newRole) {
-        /**
-         * If the newRoles is attempting to update an oldRole with an array of temporalConstraints greater than 1 throw
-         * BadRequestException. As the implementation stands now, we only support one temporal constraint per role.
-         */
-        if (isTemporalConstraintsMultiValue(newRole)) {
-            throw {code : 400, message: "Only 1 temporal constraint is supported per role."}
-        }
-        if (relationshipHelper.isRoleConditional(newRole)) {
-            newRole.members = processCreatedConditionalRole(newRole);
-        }
+    function resolveConditionalRoleMembers(role) {
+        const existingGrants = relationshipHelper.getGrants(role, 'members', 'role');
+
+        const otherGrants = existingGrants.filter(grant => !relationshipHelper.isGrantType(grant, 'conditional'));
+        const otherGrantRefs = (otherGrants || []).map(grant => grant._ref);
+
+        const conditionalGrants = searchUserIds(role.condition).filter(userId => {
+            return !otherGrantRefs.includes('managed/user/' + userId);
+        }).map(userId => {
+            return {
+                _ref: 'managed/user/' + userId,
+                _refProperties: { _grantType : 'conditional' },
+            };
+        });
+        return relationshipHelper.sortGrants([].concat(otherGrants).concat(conditionalGrants));
     }
 
-    /**
-     * This function will be called when a role's condition is removed. It will return only those
-     * @param newRole
-     */
-    function processRoleConditionRemoval(oldRole) {
-        return relationshipHelper.getConditionalAndDirectGrants(oldRole, 'members', 'role').directGrants;
-    }
-
-    /**
-     * This method will constitute the set of users which satisfy the role condition.
-     * @param role the newly-created conditional role
-     * @returns {Array} user references which satisfy the role condition
-     */
-    function processCreatedConditionalRole(role) {
-        var existingGrants = relationshipHelper.getConditionalAndDirectGrants(role, 'members', 'role'),
-            currentConditionalGrants,
-            roleMembers = [];
-
-        currentConditionalGrants = existingGrants.conditionalGrants;
-        roleMembers = roleMembers.concat(existingGrants.directGrants);
-
-        currentConditionalGrants = processUserSet(role.condition, currentConditionalGrants, appendConditionalGrants);
-        roleMembers = roleMembers.concat(currentConditionalGrants);
-        return roleMembers;
-    }
-
-    /**
-     * This function handles an update to a conditional role's condition (which includes the addition of a condition to
-     * a non-conditional role).
-     * This function will return the updated role member set. This set will be constituted as follows:
-     * 1. it will include the existing, direct role members
-     * 2. the existing, conditional role members will be mutated as follows:
-     *      a. the users who satisfied the old condition, but do not satisfy the new condition, will be removed.
-     *      b. the users who did not satisfy the old condition, but do satisfy the new condition, will be added.
-     * @param oldRole the old role
-     * @param newRole the updated role
-     * @returns {Array} the current set of role members
-     */
-    function processUpdatedConditionalRole(oldRole, newRole) {
-        var existingMembers = relationshipHelper.getConditionalAndDirectGrants(newRole, 'members', 'role'),
-            currentConditionalGrants,
-            roleGainersQuery,
-            roleLosersQuery,
-            roleMembers = [];
-
-        currentConditionalGrants = existingMembers.conditionalGrants;
-        roleMembers = roleMembers.concat(existingMembers.directGrants);
-
-        roleGainersQuery = combineAndNotFirstCondition(oldRole.condition, newRole.condition);
-        roleLosersQuery  = combineAndNotFirstCondition(newRole.condition, oldRole.condition);
-
-        currentConditionalGrants = processUserSet(roleLosersQuery, currentConditionalGrants, filterConditionalGrants);
-        currentConditionalGrants = processUserSet(roleGainersQuery, currentConditionalGrants, appendConditionalGrants);
-
-        return roleMembers.concat(currentConditionalGrants);
-    }
-
-    /**
-     * A method to combine conditions so that role updates can generate the query to determine the users which have
-     * gained, or lost, a given role grant in an updated conditional role.
-     * @param firstCondition a condition which will be negated and 'anded' with the second condition. Can be null
-     * @param secondCondition a condition which will be 'anded with the negated first condition. Can be null.
-     * @returns {*} a string which is the combination of the negated first condition, 'anded' with the second condition.
-     */
-    function combineAndNotFirstCondition(firstCondition, secondCondition) {
-        if (!isNil(firstCondition)) {
-            if (!isNil(secondCondition)) {
-                return '! (' + firstCondition + ') and ' + secondCondition;
-            } else {
-                return '! (' + firstCondition + ')';
-            }
-        } else {
-            if (!isNil(secondCondition)) {
-                return secondCondition;
-            } else {
-                throw {'error: ' : 'IllegalState: both conditions passed to combineAndNotSecondCondition nil!'};
-            }
-        }
-    }
-
-    // _.isNil is not defined in our version of lodash
-    function isNil(object) {
-        return object === undefined || object === null;
-    }
-
-    /**
-     * This method will iterate through a user-set satisfying a condition, and apply a function to mutate the list of
-     * conditional role members.
-     * @param query the query which will generate the list of users who satisfy a new role condition, or have gained
-     * or lost an updated role condition
-     * @param currentConditionalMembers the current set of conditional role members
-     * @param callbackFunction a function which takes a list of users, and the list of current conditional grants
-     * @returns {*} the currentConditionalMembers array, as updated by the callbackFunction
-     */
-    function processUserSet(query, currentConditionalMembers, callbackFunction) {
-        var pageSize = 500,
-            queryResult,
-            pagedResultsCookie;
-
-        pagedResultsCookie = "";
-        do {
-            queryResult = getUserSet(query, pageSize, pagedResultsCookie);
-            pagedResultsCookie = queryResult.pagedResultsCookie;
-            currentConditionalMembers = callbackFunction(queryResult.result, currentConditionalMembers);
-
-        } while (pagedResultsCookie);
-        return currentConditionalMembers;
-    }
-
-    /**
-     * Removes those elements from a set of role grants which correspond to user entries in the roleLosers array.
-     * @param roleLosers the array of user objects which have lost the role grant
-     * @param currentConditionalGrants the set of grant objects currently associated with the role
-     * @returns {*} the currentConditionalGrants array with the roleLosers removed.
-     */
-    function filterConditionalGrants(roleLosers, currentConditionalGrants) {
-        return _(currentConditionalGrants)
-            .reject(function (currentGrant) {
-                return _.find(roleLosers, function (roleLoser) {return 'managed/user/' + roleLoser._id === currentGrant._ref;}) !== undefined;
-            })
-            .value();
-    }
-
-    /**
-     * Adds role grant entries to the currentConditionalGrants array corresponding to users which have gained the role grant.
-     * @param roleGainers An array of users which have gained the role grant
-     * @param currentConditionalGrants the current set of conditional role grants associated with the role
-     * @returns {Array.<T>|string|*} the currentConditionalGrants array with the added role grants
-     */
-    function appendConditionalGrants(roleGainers, currentConditionalGrants) {
-        return currentConditionalGrants.concat(
-            _(roleGainers)
-                .map(function(roleGainer) {
-                    return {
-                        '_ref': 'managed/user/' + roleGainer._id, '_refProperties': {'_grantType' : 'conditional'}
-                    };
-                })
-                .value()
-        );
-    }
-
-    exports.isTemporalConstraintsMultiValue = isTemporalConstraintsMultiValue
     /**
      * Determines if a role has more than one temporal constraint.
-     *
-     * @param role the role to inspect
-     * @returns {boolean} true if temporal contraints array, inside of the role object, has a size greater than 1
+    *
+    * @param role the role to inspect
+    * @returns {boolean} true if temporal contraints array, inside of the role object, has a size greater than 1
+    */
+   function isTemporalConstraintsMultiValue(role) {
+        return role.temporalConstraints && role.temporalConstraints.length > 1;
+    }
+
+    exports.isTemporalConstraintsMultiValue = isTemporalConstraintsMultiValue;
+
+    /**
+     * TODO FIXME
      */
-    function isTemporalConstraintsMultiValue(role) {
-        if (!isNil(role.temporalConstraints)) {
-            return role.temporalConstraints.length > 1;
-        }
+    function searchUserIds(filter) {
+        const result = [];
+        let pagedResultsCookie = '';
+        do {
+            let queryResult = openidm.query('managed/user', {
+                _queryFilter: filter,
+                _pageSize: 500,
+                _pagedResultsCookie: pagedResultsCookie,
+                _fields: ['_id'],
+            });
+            result.concat(queryResult.result);
+            pagedResultsCookie = queryResult.pagedResultsCookie;
+        } while (pagedResultsCookie);
+        return result;
     }
 
     /**
-     * Determines if a role's condition has changed
-     * @param oldRole the old role
-     * @param newRole the new role
-     * @returns {boolean} true if the role condition has changed
+     * TODO FIXME
      */
-    function hasRoleConditionChanged(oldRole, newRole) {
-        return !_.isEqual(oldRole.condition, newRole.condition);
-    }
-
-    /**
-     * Queries for the set of users satisfying a condition parameter
-     * @param condition the queryFilter condition
-     * @param pageSize the size of the result set
-     * @param pagedResultsCookie used to track paging through a large result-set
-     * @returns {*|{type, title, properties}|{queryFilter, fields, sortKeys}} the queryResults
-     */
-    function getUserSet(condition, pageSize, pagedResultsCookie)  {
-        return openidm.query('managed/user', {'_queryFilter' : condition, '_pageSize' : pageSize,
-                                                '_pagedResultsCookie' : pagedResultsCookie})
+    function isRoleConditional(role) {
+        return role && role.condition !== undefined;
     }
 }());
--- openidm-zip/src/main/resources/bin/defaults/script/roles/relationshipHelper.js
+++ openidm-zip/src/main/resources/bin/defaults/script/roles/relationshipHelper.js
@@ -12,14 +12,13 @@
  * information: "Portions copyright [year] [name of copyright owner]".
  *
  * Copyright 2016 ForgeRock AS.
+ * Portions Copyright 2026 Wren Security.
  */
-
 
 /**
  * A module which defines some utility functions used to evaluate conditional roles.
  */
 (function () {
-    var _ = require('lib/lodash');
 
     /**
      * Returns the grants of either the managed role or user
@@ -30,72 +29,29 @@
      * the result of the relationship query against this particular managedObject.
      */
     exports.getGrants = function(managedObject, grantFieldName, managedObjectType) {
-        var path,
-            managedObjectId = managedObject._id;
-        if (managedObject[grantFieldName] === undefined && managedObjectId !== undefined && managedObjectId !== null) {
-            logger.trace("Managed Objects's membership collection {} is not present so querying the relationship", grantFieldName);
-            if ('user' === managedObjectType) {
-                path = getUserRolesResourcePathString(managedObjectId, grantFieldName);
-            } else if ('role' === managedObjectType) {
-                path = getRoleMembersResourcePathString(managedObjectId, grantFieldName);
-            } else {
-                throw {'error' : 'Managed Object type is neither user nor role, but ' + managedObjectType };
-            }
-            response = openidm.query(path, {"_queryId": "find-relationships-for-resource"});
-            return response.result;
-        } else {
-            return managedObject[grantFieldName] || [];
+        if (managedObject[grantFieldName]) {
+            return managedObject[grantFieldName];
         }
+        if (!managedObject._id) {
+            return [];
+        }
+        logger.trace("Managed Objects's membership collection {} is not present so querying the relationship", grantFieldName);
+        const path = org.forgerock.json.resource.ResourcePath.valueOf('managed/' + managedObjectType)
+            .child(managedObject._id).child(grantFieldName).toString();
+        return openidm.query(path, { _queryId: 'find-relationships-for-resource' }).result;
     }
 
     /**
-     * Returns the user/role grants of a managed object segmented by those conditionally assigned, and those directly assigned.
-     * param manadedObject the role or user instance
-     * param grantFieldName the name of the relationship field for the specified managed object that contains the grants.
-     * param managedObjectType the type of managed object - either 'user' or 'role'
-     * returns {{conditionalGrants: *, directGrants: *}} user/role grants, segmented by those conditionally assigned, and those directly assigned.
+     * TODO FIXME
      */
-    exports.getConditionalAndDirectGrants = function(managedObject, grantFieldName, managedObjectType) {
-        var objectGrants = this.getGrants(managedObject, grantFieldName, managedObjectType);
-
-        return {
-            'conditionalGrants' : _.filter(objectGrants, isGrantConditional),
-            'directGrants' : _.reject(objectGrants, isGrantConditional)
-        };
-    }
-
-    function getUserRolesResourcePathString(userId, rolesPropName) {
-        return org.forgerock.json.resource.ResourcePath.valueOf("managed/user").child(userId).child(rolesPropName).toString();
-    }
-
-    function getRoleMembersResourcePathString(roleId, membersPropName) {
-        return org.forgerock.json.resource.ResourcePath.valueOf("managed/role").child(roleId).child(membersPropName).toString();
+    exports.isGrantType = function(grant, type) {
+        return grant && grant._refProperties && grant._refProperties._grantType === type;
     }
 
     /**
-     *
-     * @returns {boolean|*|Object|n} the list of existing conditional roles
+     * TODO FIXME
      */
-    exports.getConditionalRoles = function() {
-        return openidm.query('managed/role', {_queryFilter: '/condition pr'}).result;
-    }
-
-    /**
-     * Determines whether a user or role grant is conditional
-     * @param grant the role or user role grant
-     * @returns {boolean} true if the grant is conditional, false otherwise
-     */
-    function isGrantConditional(grant) {
-        var grantProperties = grant['_refProperties'];
-        return grantProperties !== undefined && grantProperties !== null && grantProperties._grantType === 'conditional';
-    }
-
-    /**
-     *
-     * @param role the role
-     * @returns {boolean} returns true if the role is conditional - false otherwise.
-     */
-    exports.isRoleConditional = function(role) {
-        return role.condition !== undefined;
+    exports.sortGrants = function(grants) {
+        return grants.sort((a, b) => a._ref.localeCompare(b._ref));
     }
 }());
